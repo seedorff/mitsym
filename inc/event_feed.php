c715e0b8647de6173ebf911452b4d61fd624a522
seedorff <seedorff.himself@gmail.com>
2016-05-20 12:12:12 +0200

    event feed, event type

--- inc/event_feed.php
+++ inc/event_feed.php
@@ -2,17 +2,18 @@
   <h3 class="card-title">Arrangementer</h3>
     <p class="card-text">Alle kommende arrangementer. Både åbne og interne.</p>
 
-    <!-- Event loop - for each -->
+    <!-- Event loop -->
     <div class="row event">
       <div class="col-xs-3 event-meta">
         <span class="event-date">10</span>
         <span class="event-month">Maj</span>
       </div>
       <div class="col-xs-9 event-description">
-        Netværksgruppemøde: Overvejelser fra en marketingschef.
+        <h4>Netværksgruppemøde: Overvejelser fra en marketingschef.</h4>
+        <div class="event-type">Åbent for alle</div>
       </div>
     </div>
-    <!-- /Event loop - for each  -->
+    <!-- /Event loop -->
     
     <div class="row event">
       <div class="col-xs-3 event-meta">
