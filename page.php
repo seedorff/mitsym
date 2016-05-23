bd230baaff98aa882ccd9d2ede53f4e2c9f2d119
seedorff <seedorff.himself@gmail.com>
2016-05-23 13:02:05 +0200

    page styling

--- page.php
+++ page.php
@@ -1,26 +1,28 @@
 <div class="row">
-	<div class="col-md-8">
-  		<h1>Standard sideskabelon</h1>
-  		<p>Standard skabelon til indholdsside af anden karakter.</p>
+	<div class="col-md-8 main-content">
+		<div class="card card-block">
+	  		<h1>Standard sideskabelon</h1>
+	  		<p>Standard skabelon til indholdsside af anden karakter.</p>
 
-		<p>Vises også pt for sider der endnu ikke har deres egen template.</p>
+			<p>Vises også pt for sider der endnu ikke har deres egen template.</p>
 
-  		<p>Lorem ipsum dolor sit amet, mel impedit corpora constituto no, qui an commune postulant rationibus. Nec saperet alienum apeirian ut, ubique singulis te nam. Dicunt argumentum vel ea, cum ea tamquam maiorum, an audiam dolorum qui. Sed scripta diceret sadipscing ea. At duo sumo explicari corrumpit. Est eu aliquam vulputate dissentias.
-  		  </p>
-  		  <img src="img/business.jpg" alt="Placeholder img" class="img-fluid">
-  		  <p>
-  		An mea legere integre nostrum. Id invidunt efficiantur ius, everti denique eos ad. Purto dignissim gloriatur et vim, nam no tota electram. At possim hendrerit sadipscing vis, pro cu epicuri luptatum. Ius omnesque voluptaria et.
-  		  </p>
-  		  <p>
-  		Option interesset qui at, graece inimicus ad vis, affert oratio periculis ne est. Mea ne bonorum appetere assueverit, ancillae phaedrum mea at. Nihil ceteros constituto ne pri, cum odio tincidunt ea, aeque movet eu vel. Vero mediocritatem no has, ea pri malorum percipit, sed ad zril laoreet.
-  		  </p>
-  		  <h2>Overskrift 2</h2>
-  		  <p>
-  		Mutat eirmod tacimates per ea, sea te falli nobis. Legimus albucius repudiare ei vim, eu habeo fierent detracto qui. Et usu case dictas dissentiunt. Ei pri sonet ceteros deserunt. At sea dicta accommodare, sed in vocibus minimum recteque.
-  		  </p>
-  		  <p>
-  		Te dicta euripidis vituperata vis, no elit assum tantas duo. In adipisci philosophia eam, dicat latine nam at. Eum ut viderer pericula, ei patrioque abhorreant dissentiet his, consequuntur mediocritatem sea ea. Sed sale saepe consequat no, soleat percipitur contentiones id per. Eos ea justo atqui. Ex facete molestie placerat mei, cu eius diceret aliquando eos. Nec ne liber efficiantur.
-  		</p>
+	  		<p>Lorem ipsum dolor sit amet, mel impedit corpora constituto no, qui an commune postulant rationibus. Nec saperet alienum apeirian ut, ubique singulis te nam. Dicunt argumentum vel ea, cum ea tamquam maiorum, an audiam dolorum qui. Sed scripta diceret sadipscing ea. At duo sumo explicari corrumpit. Est eu aliquam vulputate dissentias.
+	  		  </p>
+	  		  <img src="img/business.jpg" alt="Placeholder img" class="img-fluid">
+	  		  <p>
+	  		An mea legere integre nostrum. Id invidunt efficiantur ius, everti denique eos ad. Purto dignissim gloriatur et vim, nam no tota electram. At possim hendrerit sadipscing vis, pro cu epicuri luptatum. Ius omnesque voluptaria et.
+	  		  </p>
+	  		  <p>
+	  		Option interesset qui at, graece inimicus ad vis, affert oratio periculis ne est. Mea ne bonorum appetere assueverit, ancillae phaedrum mea at. Nihil ceteros constituto ne pri, cum odio tincidunt ea, aeque movet eu vel. Vero mediocritatem no has, ea pri malorum percipit, sed ad zril laoreet.
+	  		  </p>
+	  		  <h2>Overskrift 2</h2>
+	  		  <p>
+	  		Mutat eirmod tacimates per ea, sea te falli nobis. Legimus albucius repudiare ei vim, eu habeo fierent detracto qui. Et usu case dictas dissentiunt. Ei pri sonet ceteros deserunt. At sea dicta accommodare, sed in vocibus minimum recteque.
+	  		  </p>
+	  		  <p>
+	  		Te dicta euripidis vituperata vis, no elit assum tantas duo. In adipisci philosophia eam, dicat latine nam at. Eum ut viderer pericula, ei patrioque abhorreant dissentiet his, consequuntur mediocritatem sea ea. Sed sale saepe consequat no, soleat percipitur contentiones id per. Eos ea justo atqui. Ex facete molestie placerat mei, cu eius diceret aliquando eos. Nec ne liber efficiantur.
+	  		</p>
+  		</div>
  	</div>
  	<div class="col-md-4">
  		<div class="card card-block">
