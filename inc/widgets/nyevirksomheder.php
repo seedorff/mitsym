695153c05b058394b2a3b3ba0456047855f43210
seedorff <seedorff.himself@gmail.com>
2016-05-25 14:38:14 +0200

    feed meta

--- inc/widgets/nyevirksomheder.php
+++ inc/widgets/nyevirksomheder.php
@@ -15,7 +15,7 @@ $data=json_decode($json,true);
           <h4><a href="<?php echo $site_url; ?>/?company-id=<?php echo $company['id']; ?>"><?php echo $company['company']; ?></a>
           </h4>
           <div class="feed-meta text-muted">
-            Indflyttet <?php echo $company['move-in-date']; ?>
+            Indflyttet<i class="fa fa-caret-right text-muted"></i><?php echo $company['move-in-date']; ?><i class="fa fa-home text-muted"></i><?php echo $company['location']; ?>
           </div>
       </div>
     </div>
