7d6750c02fd064480bdc277021672e06f212b052
seedorff <seedorff.himself@gmail.com>
2016-05-25 09:35:00 +0200

    opslagstavle feed adjust

--- inc/widgets/opslagstavle_feed.php
+++ inc/widgets/opslagstavle_feed.php
@@ -13,7 +13,7 @@ $data=json_decode($json,true);
     foreach($data as $key => $post){ ?>
       <div class="post"> 
 
-          <div class="post-meta"><?php echo $post['post-date']; ?></div>
+          <div class="post-meta"><?php echo strstr( $post['author'] . ' ', ' ', true ); ?><i class="fa fa-caret-right text-muted"></i><?php echo $post['posted-in']; ?><i class="fa fa-clock-o text-muted"></i><?php echo $post['post-date']; ?></div>
           <h4><a href="<?php echo $site_url; ?>/?post-id=<?php echo $post['id']; ?>"><?php echo $post['post-title']; ?></a></h4>
 
       </div>
