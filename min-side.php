80f1542bfd8bb8402567a845969c1b33b50815b8
seedorff <seedorff.himself@gmail.com>
2016-05-25 15:04:18 +0200

    column bug fix

--- min-side.php
+++ min-side.php
@@ -1,23 +1,21 @@
-<div class="row">
-  <div class="col-xs-12 card-columns">
-    <div class="card card-block">
-        <h3 class="card-title">Printforbrug</h3>
-        <p class="card-text">Se dit printforbrug.</p>
-        <a href="<?php echo $site_url; ?>?page=printforbrug" class="btn btn-secondary">Printforbrug</a>
-    </div>
-
-    <div class="card card-block">
-      <h3 class="card-title">Din virksomhed</h3>
-      <p class="card-text">Se oplysninger for din virksomhed og dine medarbejdere.</p>
-      <a href="<?php echo $site_url; ?>?page=sestamdata" class="btn btn-secondary">Stamdata</a>
-      <a href="<?php echo $site_url; ?>?page=medarbejdere" class="btn btn-secondary">Medarbejdere</a>
-    </div>
-
-    <div class="card card-block">
-      <h3 class="card-title">Dine bookinger</h3>
-      <p class="card-text">Se dine planlagte bookinger</p>
-      <a href="<?php echo $site_url; ?>?page=booking" class="btn btn-secondary">Se Bookinger</a>
-    </div>
-
+<div class="card-columns">
+  <div class="card card-block">
+      <h3 class="card-title">Printforbrug</h3>
+      <p class="card-text">Se dit printforbrug.</p>
+      <a href="<?php echo $site_url; ?>?page=printforbrug" class="btn btn-secondary">Printforbrug</a>
   </div>
+
+  <div class="card card-block">
+    <h3 class="card-title">Din virksomhed</h3>
+    <p class="card-text">Se oplysninger for din virksomhed og dine medarbejdere.</p>
+    <a href="<?php echo $site_url; ?>?page=sestamdata" class="btn btn-secondary">Stamdata</a>
+    <a href="<?php echo $site_url; ?>?page=medarbejdere" class="btn btn-secondary">Medarbejdere</a>
+  </div>
+
+  <div class="card card-block">
+    <h3 class="card-title">Dine bookinger</h3>
+    <p class="card-text">Se dine planlagte bookinger</p>
+    <a href="<?php echo $site_url; ?>?page=booking" class="btn btn-secondary">Se Bookinger</a>
+  </div>
+
 </div>
